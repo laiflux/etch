f1e72b74e7b9f3c2f146b50dd27ac937f13f2879
lai <255363213+laiflux@users.noreply.github.com>
2026-02-14 20:47:41 +0530

    Add color change functionality and recast button

--- script.js
+++ script.js
@@ -1,36 +1,72 @@
 let gridContainer = document.querySelector('.container');
-let square;
+
 
 for (let i = 0; i < 16; i++) {
     for (let j = 0; j < 16; j++) {
-        square = document.createElement('div');
+       const square = document.createElement('div');
         square.className = 'square';
         gridContainer.appendChild(square);
 
+        let squareInteractionCount = 0;
+
 square.addEventListener('mouseenter', (e) => {
+    
     let r = Math.floor(Math.random() * 256);
     let g = Math.floor(Math.random() * 256);
     let b = Math.floor(Math.random() * 256);
     
     e.target.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;  
+    
 });
-    }}
+
+square.addEventListener('mouseover', () => {
+        squareInteractionCount++;
+        let newOpacity = 1 - (squareInteractionCount * 0.1);
+        square.style.opacity = Math.max(newOpacity, 0);
+        });
+    }
+}
+
 
 const recastBtn = document.getElementById('recast');
 
 recastBtn.addEventListener('click', () => {
-    let newGrid = +prompt("please enter the number of squares per side for the new grid");
+    let newGridLength = +prompt("please enter the number of squares per side for the new grid");
 
     gridContainer.innerHTML = "";
-    let newSquare;
-    for (let i = 0; i < newGrid; i++) {
-        for (let j = 0; j < newGrid; j++) {
-            newSquare = document.createElement('div');
-            newSquare.className = "square";
+    let newGridSquare = 100 / newGridLength;
+
+    for (let i = 0; i < newGridLength * newGridLength; i++) {
+        if (isNaN(newGridLength) || newGridLength <= 0 || newGridLength > 100) {
+            alert("please enter digits. no more than 100. or less than 1.");
+            return;
+        } else if (newGridLength === null) {
+            return; 
+        } else {
+            const newSquare = document.createElement('div');
+            newSquare.className = "newSquare";
+            newSquare.style.flexBasis = `${newGridSquare}%`;
+            newSquare.style.height = `${newGridSquare}%`;
+            newSquare.style.border = "0.01rem solid #444";
+            
             gridContainer.appendChild(newSquare);
-        }
+
+            let interactionCount = 0;
+
+    newSquare.addEventListener('mouseenter', (e) => {
+        let r = Math.floor(Math.random() * 256);
+        let g = Math.floor(Math.random() * 256);
+        let b = Math.floor(Math.random() * 256);
+    
+    e.target.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;  
+});
+    newSquare.addEventListener('mouseover', () => {
+        interactionCount++;
+        let newOpacity = 1 - (interactionCount * 0.1);
+        
+        newSquare.style.opacity = Math.max(newOpacity, 0);
+    });
     }
+}
 })
-
-
 
